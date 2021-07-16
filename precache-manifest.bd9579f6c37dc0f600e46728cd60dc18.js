1f91f7bf9cd07738f46b47eacf063e4b9bf52725
arielyiyitou <my442@drexel.edu>
2021-07-16 01:24:16 -0400

    Updates

---
+++ precache-manifest.bd9579f6c37dc0f600e46728cd60dc18.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2756e8bd6512cec78b8b864af789974c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3eccb7a218060e0f14d5",
+    "url": "/static/css/main.956fad23.chunk.css"
+  },
+  {
+    "revision": "7ab3694f172b81390336",
+    "url": "/static/js/2.efdfa384.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/static/js/2.efdfa384.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3eccb7a218060e0f14d5",
+    "url": "/static/js/main.a83b2ce2.chunk.js"
+  },
+  {
+    "revision": "f52ba179432c3be28857",
+    "url": "/static/js/runtime-main.554504a0.js"
+  },
+  {
+    "revision": "939d7f9a43eb23c048f5e1940fc37857",
+    "url": "/static/media/ariel.939d7f9a.jpg"
+  },
+  {
+    "revision": "c07e963a3de0f3d068aa57f7b7cfe022",
+    "url": "/static/media/drexel.c07e963a.png"
+  },
+  {
+    "revision": "9d3da0e22086c40d5e3106d01c0f6402",
+    "url": "/static/media/resume.9d3da0e2.pdf"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/home/precache-manifest.f73bea89c7ac4df93262cfdb1522c5c0.js"
+  "/precache-manifest.bd9579f6c37dc0f600e46728cd60dc18.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/home/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
